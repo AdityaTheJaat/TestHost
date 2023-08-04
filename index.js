08848f7d88d7785afb654adf2e04ea8c11211fb0
AdityaTheJaat <programmingID1306@gamil.com>
2023-08-04 12:33:21 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res)=> {
+  res.send("Hosted Successfully!")
+})
+
+app.listen(6000, () => {
+  console.log("App is running on 6000!")
+})
